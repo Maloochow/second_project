4bcff912fc771eb6606d2a83dd4221d2120b890a
Maloochow <luyao.mly@gmail.com>
2020-07-28 17:57:48 -0400

    add welcome page imge

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -5,3 +5,16 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+      
